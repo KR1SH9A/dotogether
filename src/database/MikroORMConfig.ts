e34497e7bed8c6e7b86811dc41f9c537942fd60f
KR1SH9A <krishnamonidas0001@gmail.com>
2026-05-07 22:39:33 +0530

    removed the cache generation/metadata file referances for a cleaner approach for build

--- src/database/MikroORMConfig.ts
+++ src/database/MikroORMConfig.ts
@@ -1,12 +1,11 @@
 //MikroORM setup with the database and entity discovery for migrations
 import "dotenv/config";
+
 import { defineConfig } from "@mikro-orm/postgresql";
 import { Migrator } from "@mikro-orm/migrations";
 import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
-import { GeneratedCacheAdapter } from "@mikro-orm/core";
-import fs from 'fs';
 
-//manually introducing entities for now,
+// entities
 import { User } from "../features/auth/User.entity.js";
 import { Session } from "../features/auth/Session.entity.js";
 import { FriendRequest } from "../features/friend/FriendRequest.entity.js";
@@ -14,7 +13,10 @@ import { Todo } from "../features/todo/Todo.entity.js";
 
 export default defineConfig({
   dbName: process.env.DATABASE_NAME || "dummy_db",
-  clientUrl: process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost:5432/dummy",
+
+  clientUrl:
+    process.env.DATABASE_URL ||
+    "postgresql://dummy:dummy@localhost:5432/dummy",
 
   driverOptions: {
     ssl: {
@@ -23,15 +25,8 @@ export default defineConfig({
   },
 
   metadataProvider: TsMorphMetadataProvider,
-  metadataCache: { 
-    enabled: process.env.NODE_ENV === "production",
-    ...(process.env.NODE_ENV === "production" ? { adapter: GeneratedCacheAdapter } : {}),
-    options: process.env.NODE_ENV === "production" 
-      ? { data: JSON.parse(fs.readFileSync(process.cwd() + '/temp/metadata.json', 'utf8')) } 
-      : { cacheDir: process.cwd() + '/temp' }
-  },
+
   entities: [User, Session, FriendRequest, Todo],
-  // entitiesTs: ["src/features/**/*.entity.ts"],
 
   debug: process.env.NODE_ENV !== "production",
 

4dca9f7d363b591f8675b11908ad5a877b1b31f0
KR1SH9A <krishnamonidas0001@gmail.com>
2026-05-07 22:28:23 +0530

    dependencies for render build

--- temp/test-cors.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import cors from 'cors';
-const app = express();
-app.use(cors({ origin: true, credentials: true }));
-app.get('/', (req, res) => res.json({ok: true}));
-app.listen(3001, () => console.log('Listening'));
